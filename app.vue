d8845342d537cefc0f8e876b70392473885ea8ea
岡本紗也加 <sayasurvey@yahoo.co.jp>
2024-06-16 14:54:08 +0900

    ADD: pdfビューワーの実装

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <vue-pdf-app style="height: 100vh;" pdf="https://drive.google.com/file/d/1V35ETdOd-fkgzP-v4lxulb_Tk4L7m7vU/view?usp=sharing"></vue-pdf-app>
 </template>
+
+<script>
+import VuePdfApp from "vue3-pdf-app";
+import "vue3-pdf-app/dist/icons/main.css";
+
+export default {
+  components: {
+    VuePdfApp
+  }
+};
+</script>
