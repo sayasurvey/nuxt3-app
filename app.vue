4e2ab1916386a215c38cd8bf24d1c42f11ed7713
sayasurvey <sayasurvey@yahoo.co.jp>
2025-02-21 00:42:53 +0900

    ADD: バリテーション機能

--- app.vue
+++ app.vue
@@ -1,22 +1,23 @@
 <template>
-  <div style="height: 100vh;">
-    <vue-pdf-app :pdf="pdfPath"></vue-pdf-app>
+  <div>
+    <Form :validationSchema="validationSchema" v-slot="{ meta: { valid } }">
+      <TextField v-model="email" name="email" label="メールアドレス"/>
+      <TextField v-model="password" name="password" label="パスワード" inputType="password"/>
+      <button :disabled="!valid">ログイン</button>
+    </Form>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { Form } from 'vee-validate';
+import * as yup from 'yup';
 import { ref } from 'vue';
-import VuePdfApp from 'vue3-pdf-app';
-import 'vue3-pdf-app/dist/icons/main.css';
 
-// Correct path without '/public'
-const pdfPath = ref('/materials006_nuri1.pdf');
-</script>
+const email = ref("");
+const password = ref("");
 
-<script>
-export default {
-  components: {
-    VuePdfApp
-  }
-};
+const validationSchema = yup.object().shape({
+  email: yup.string().required().email().label("メールアドレス"),
+  password: yup.string().required().min(8).label("パスワード")
+})
 </script>
